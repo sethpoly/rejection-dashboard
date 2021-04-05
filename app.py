263591e1e249eb4b1d7415be4abf39a2222c5a6b
sethpoly <emailspolyniak@gmail.com>
2021-04-05 11:57:53 -0400

    Added application per month graph

--- app.py
+++ app.py
@@ -3,18 +3,39 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-
+import plotly.express as px
 
 # Connect to sheets API and retrieve latest sheet
-data_sheet = acc.Spreadsheet('Applications', 'Main').sheet
+data = acc.Spreadsheet('Applications', 'Main').sheet
 
 # Convert google sheet to csv for pandas use
-rows = data_sheet.get_all_values()
-df = pd.DataFrame.from_records(rows)
-print(df)
+rows = data.get_all_records()
+df = pd.DataFrame(rows)
+df["dateApplied"] = pd.to_datetime(df["dateApplied"], infer_datetime_format=True)
 
 # Init Dash app
 app = dash.Dash(__name__)
 
-# Define layout property of app
+fig = px.scatter(df, x=df["dateApplied"].unique(), y=df.groupby(['dateApplied']).size(),
+                 size_max=60,
+                 labels={
+                     "x": "Date Applied",
+                     "y": "Applications Sent"
+                 },
+                 title="Applications Sent Per Day")
 
+# Define layout property of app
+app.layout = html.Div(
+    children=[
+        html.H1(children="Rejection Analytics", ),
+        html.P(
+            children="Analyze the responses of every job I've ever applied to",
+        ),
+        dcc.Graph(
+            figure=fig
+        )
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
