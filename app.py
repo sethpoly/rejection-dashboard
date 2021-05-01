c55742657ff3a1217bdfc8d810c97a18340014c5
sethpoly <emailspolyniak@gmail.com>
2021-05-01 18:06:01 -0400

    Removed unneeded imports

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
-from dash.dependencies import Input, Output
 
 # Init Dash app
 app = dash.Dash(__name__,
