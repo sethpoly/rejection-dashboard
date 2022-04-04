8ca7f9a8b22e64c22ca338d958d31f4e0a1bce7b
sethpoly <emailspolyniak@gmail.com>
2022-04-04 08:28:30 -0400

    add google_key.json env builder

--- service_account.py
+++ service_account.py
@@ -7,7 +7,7 @@ def create_keyfile_dict():
         "type": os.environ.get("SHEET_TYPE"),
         "project_id": os.environ.get("SHEET_PROJECT_ID"),
         "private_key_id": os.environ.get("SHEET_PRIVATE_KEY_ID"),
-        "private_key": os.environ.get("SHEET_PRIVATE_KEY"),
+        "private_key": os.environ.get("SHEET_PRIVATE_KEY").replace('\\n', '\n'),
         "client_email": os.environ.get("SHEET_CLIENT_EMAIL"),
         "client_id": os.environ.get("SHEET_CLIENT_ID"),
         "auth_uri": os.environ.get("SHEET_AUTH_URI"),
